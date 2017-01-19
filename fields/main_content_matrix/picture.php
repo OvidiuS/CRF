f0a3ffa1e09c408fe8a838877f16aff3a60ab1bd
Ovi Savescu <savescu.ovidiu@gmail.com>
2017-01-19 22:34:04 +0200

    Added main content matrix and styles for its images

---
+++ fields/main_content_matrix/picture.php
@@ -0,0 +1,28 @@
+<?php $options = array(
+  'quality' => 90,
+  'upscaling' => false
+); ?>
+
+<?php switch ($page->image_alignment_matrix) {
+	case '1':
+		$imageWidth = 291;
+		break;
+	case '2':
+		$imageWidth = 291;
+		break;
+	case '3':
+		$imageWidth = 367;
+		break;
+	case '4':
+		$imageWidth = 582;
+		break;
+	default:
+		
+		
+		break;
+} ?>
+
+<figure class="<?= $page->image_alignment_matrix->value ?>">
+    <img src='<?= $page->image_matrix->width($imageWidth, $options)->url ?>' alt='<?= $page->image_matrix->description ?>' />
+    <figcaption><?= $page->title ?></figcaption>
+</figure>
---
+++ fields/main_content_matrix/rich_text.php
@@ -0,0 +1,1 @@
+<?php echo $page->rich_text; ?>
