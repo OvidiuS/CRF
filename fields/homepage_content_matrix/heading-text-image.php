19ced8efe60980d04d337e9627309ec57a0740ff
Ovi Savescu <savescu.ovidiu@gmail.com>
2017-01-25 17:09:36 +0200

    fixed text alignment in homepage matrix

--- fields/homepage_content_matrix/heading-text-image.php
+++ fields/homepage_content_matrix/heading-text-image.php
@@ -1,5 +1,5 @@
 							<div class="home-content-block clearfix">
 								<img class="<?= $page->left_right?>" src="<?= $page->image_matrix->first()->width(371)->url ?>">
 								<h2 class=""><?= $page->title ?></h2>
-								<?= $page->rich_text; ?>
+								<div class="<?= $page->left_right == 'left' ? 'right' : 'left' ?>"> <?= $page->rich_text; ?></div>
 							</div>
