420aee32932ae0ea4de10ad7bf3936cafb2570ef
Ovi Savescu <savescu.ovidiu@gmail.com>
2017-01-26 12:58:15 +0200

    fixed float & crop for homepage content imgs

--- fields/homepage_content_matrix/heading-text-image.php
+++ fields/homepage_content_matrix/heading-text-image.php
@@ -1,5 +1,5 @@
 							<div class="home-content-block clearfix">
-								<img class="<?= $page->left_right?>" src="<?= $page->image_matrix->first()->width(371)->url ?>">
+								<img class="<?= $page->left_right->title?>" src="<?= $page->innerPageBanner->getCrop("homepage_image")->url ?>">
 								<h2 class=""><?= $page->title ?></h2>
-								<div class="<?= $page->left_right == 'left' ? 'right' : 'left' ?>"> <?= $page->rich_text; ?></div>
+								<div class="<?= $page->left_right->title == 'left' ? 'right' : 'left' ?>"> <?= $page->rich_text; ?></div>
 							</div>
