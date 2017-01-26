b8da746b640335ef2b2572de2c1aeddb07c17155
Ovi Savescu <savescu.ovidiu@gmail.com>
2017-01-26 17:07:13 +0200

    search page v1

--- search.php
+++ search.php
@@ -16,7 +16,7 @@ if($q = $sanitizer->selectorValue($input->get->q)) {
 	// Search the title, body and sidebar fields for our query text.
 	// Limit the results to 50 pages. 
 	// Exclude results that use the 'admin' template. 
-	$matches = $pages->find("title|body|sidebar~=$q, limit=50"); 
+	$matches = $pages->find("title|body|main_content_matrix.rich_text~=$q,template!=blog-post,limit=50"); 
 
 	$count = count($matches); 
 
@@ -25,7 +25,25 @@ if($q = $sanitizer->selectorValue($input->get->q)) {
 			"<ul class='nav'>";
 
 		foreach($matches as $m) {
-			$out .= "<li><p><a href='{$m->url}'>{$m->title}</a><br />{$m->summary}</p></li>";
+			if ($m->main_content_matrix ) {
+				foreach ($m->main_content_matrix as $matrix_block) {
+					if ($matrix_block->type == "rich_text") {
+						$m_summary = $matrix_block->rich_text;
+						break;
+					}
+				}
+				$m_summary = strip_tags($m_summary);
+				$limit = 150;
+				if(strlen($m_summary) >= $limit){
+					$m_summary = substr($m_summary, 0, $limit);
+					$pos = strrpos($m_summary, " ");
+					if ($pos>0) {
+				        $m_summary = substr($m_summary, 0, $pos);
+				    }
+				}
+			}
+
+			$out .= "<li><p><a href='{$m->url}'>{$m->title}</a><br />{$m_summary}...</p></li>";
 		}
 
 		$out .= "</ul>";
@@ -34,7 +52,7 @@ if($q = $sanitizer->selectorValue($input->get->q)) {
 		$out .= "<h2>Sorry, no results were found.</h2>";
 	}
 } else {
-	$out .= "<h2>Please enter a search term in the search box (upper right corner)</h2>";
+	$out .= "<h2>Please enter a search term in the search box</h2>";
 }
 
 // Note that we stored our output in $out before printing it because we wanted to execute
@@ -42,8 +60,23 @@ if($q = $sanitizer->selectorValue($input->get->q)) {
 // displays the current search query in the search box (via the $input->whitelist) and 
 // we wanted to make sure we had that setup before including the header template. 
 
-include("./head.inc"); 
 
-echo $out; 
+include($config->paths->templates."includes/head.inc");?>
 
-include("./foot.inc"); 
+<body>
+<? include($config->paths->templates."includes/header.inc"); ?>
+					
+					<div class="singleColumnWrapper">
+						<h1>Search</h1>
+						<form id='search_form' action='<?php echo $config->urls->root?>search/' method='get'>
+							<input type='text' name='q' id='search_query' value='<?php echo htmlentities($input->whitelist('q'), ENT_QUOTES, 'UTF-8'); ?>' />
+							<button type='submit' id='search_submit'>Search</button>
+						</form>
+						<?php echo $out; ?>
+						<br/><br/><br/><br/><br/><br/>
+					</div>
+					<!--end singleColumnWrapper--> 
+</div>
+<? include($config->paths->templates."includes/foot.inc"); ?>
+</body>
+</html>
